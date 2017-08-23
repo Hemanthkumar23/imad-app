3fd3615577457279bfaeaaa91492175f772e259f
Hemanth kumar <30660317+Hemanthkumar23@users.noreply.github.com>
2017-08-23 21:30:56 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -20,9 +20,9 @@ var submit = document.getElementById('submit_btt');
 submit.onclick = function () {
 var names = ['name1','name2','name3','name4'];
 var list = '';
-for (var i=0; i < names.length; i++) {
+for (var i=0; i< names.length; i++) {
     list += '<li>' + names[i] + '</li>';
 }
 var ul = document.getElementById('namelist');
-ui.innerHTML = list;
+ul.innerHTML = list;
 };
