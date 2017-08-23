ae0bffdf1a2b5ec0b8d53459a0313c1ab12fb18e
Hemanth kumar <30660317+Hemanthkumar23@users.noreply.github.com>
2017-08-23 20:25:53 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -10,3 +10,9 @@ function moveRight () {
 img.onclick = function() { 
     var interval = setInterval(moveRight, 50);
     };
+var button=document.getElementById('counter');
+button.onclick = function () { 
+     counter = counter + 1;
+     var span = document.getElementById('count');
+     span.innerHtml = counter.toString(); 
+};    
