97d894c25d469687211b559b65579b61aee4dabb
Hemanth kumar <30660317+Hemanthkumar23@users.noreply.github.com>
2017-08-23 20:43:41 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,15 @@ var button = document.getElementById('counter');
 var counter = 0;
 button.onclick = function () { 
     var request = new XMLHttpRequest();
-     counter = counter + 1;
+    request.onreadystatechange = function () {
+        if (request.readyState === XMLHttpRequest.DONE) {
+            if (request.status === 200) {
+            var counter = request.responseText;
      var span = document.getElementById('count');
      span.innerHTML = counter.toString(); 
+            }
+            }
+    };
+  request.open('GET','http://http://phemanthkumar23.imad.hasura-app.io/counter',true);
+  request.send(null);
 };    
