b27dd6266ffe1680623be4ede9465223cef5c5b2
Hemanth kumar <30660317+Hemanthkumar23@users.noreply.github.com>
2017-08-23 18:45:25 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,9 +4,9 @@ element.innerHTML='New value';
 var img=document.getElementById('madi');
 var marginLeft=0;
 function moveRight () {
-    marginLeft=marginLeft+10;
-    img.style.marginLeft=marginLeft + 'px';
+    marginLeft = marginLeft + 10;
+    img.style.marginLeft = marginLeft + 'px';
 }
 img.onclick = function() { 
-    var interval = setInterval(moveRight,100);
+    var interval = setInterval(moveRight, 100);
     };
