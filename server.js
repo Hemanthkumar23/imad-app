bcfc46d78fa68f296e0a1decf05cd7f6671e1a03
Hemanth kumar <30660317+Hemanthkumar23@users.noreply.github.com>
2017-08-31 22:51:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -79,6 +79,38 @@ app.post('/create-user', function(req, res) {
    });
 });
 
+app.post('/login', function(req, res) {
+   var username = req.body.username;
+   var password = req.body.password;
+   
+   var salt=crypto.randomBytes(128).toString('hex');
+   var dbstring = hash(password, salt); 
+   pool.query('SELECT * FROM "user" WHERE username = $1', [username], function(err, result) {
+       if(err)  {
+    res.status(500).send(err.toString());
+  }
+  else
+  { if(result.rows.length === 0) {
+      res.send(403).send('Username/password is invalid');
+    } 
+   else
+  {   var dbString=result= rows[0].password;
+      var salt=dbString.split('$')[2];
+      var hashedPassword = hash(password, salt);
+      if(hashedPassword === dbsString ) {
+      res.send('Crendiatls correct'); 
+      }
+      else {
+      }
+      }
+      
+      
+  }
+ }
+ 
+   });
+});
+
 
 var pool = new Pool(config);
 app.get('/test-do', function (req, res) {
