0906d06458af02d98a098b05850d68d0ebcc438c
Hemanth kumar <30660317+Hemanthkumar23@users.noreply.github.com>
2017-08-31 21:01:17 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
+var crypto=require('crypto');
 var config = {
     user:'phemanthkumar23',
     database:'phemanthkumar23',
@@ -49,6 +50,16 @@ return htmlTemplate;
 app.get('/',function(req,res) {
     res.sendFile(path.join(__dirname,'ui','index.html'));
 });
+function hash (input) {
+    var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
+    return hashed.toString('hex');
+}
+
+app.get('/hash/:input', function(req, res) {
+    var hashedString = hash(input.params.input, 'this-is-some-random-string');
+    res.send(hashedString);
+});
+
 var pool = new Pool(config);
 app.get('/test-do', function (req, res) {
 pool.query('select * from test',function(err, result) {
