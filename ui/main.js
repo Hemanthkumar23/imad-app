dacc8b882716192360c7deba7fffc20e4d469537
Hemanth kumar <30660317+Hemanthkumar23@users.noreply.github.com>
2017-08-31 23:10:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,26 +1,26 @@
 
 var submit = document.getElementById('submit_btt');
-submit.onclick = function () {
-
-};
 submit.onclick = function () { 
     var request = new XMLHttpRequest();
     request.onreadystatechange = function () {
         if (request.readyState === XMLHttpRequest.DONE) {
             if (request.status === 200) {
-            var names = request.responseText;
-            names = JSON.parse(names);
-var list = '';
-for (var i=0; i< names.length; i++) {
-    list += '<li>' + names[i] + '</li>';
-}
-var ul = document.getElementById('namelist');
-ul.innerHTML = list;
+                console.log('user logged in');
+                aletr('logged in successfully');
             }
+            else if(request.status === 403) {
+                alert('username/password is incorrect');
+            } else if(request.status === 500) {
+                alert('Something went wrong on server');
+                
             }
+        }
     };
-  var nameInput=document.getElementById('name');
-  var name = nameInput.value;
-  request.open('GET','http://phemanthkumar23.imad.hasura-app.io/submit-name?name=' + name, true);
-  request.send(null);
+  var username=document.getElementById('username').value;
+  var password=document.getElementById('password').value;
+  console.log(username);
+  console.log(password);
+  request.open('POST','http://phemanthkumar23.imad.hasura-app.io/submit-name?name=' + name, true);
+  request.setRequestHeader('conetnt-type', 'application/json');
+  request.send(JSON.stringify({username:username,password: password}));
 };
